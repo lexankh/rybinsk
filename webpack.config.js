abd47985cff191de094dc1b185f66293e86a73ae
Алексей Хорошавин <aleksejhorosavin@Air-Aleksej.Dlink>
2020-11-13 18:07:11 +0400

    3 blocks doned

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+var ImageminPlugin = require('imagemin-webpack-plugin').default;
+const isDev = process.env.NODE_ENV === 'development';
+
+
+module.exports = {
+    entry: {
+        main: './src/index.js',
+        
+
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|svg|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                            esModule: false,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                loader: 'file-loader?name=./vendor/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ 
+            filename: '[name].[contenthash].css',
+        }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                preset: ['default'],
+            },
+            canPrint: true
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: './src/index.html',
+            chunks: ['main'],
+            filename: 'index.html'
+        }),
+        new ImageminPlugin({
+            disable: process.env.NODE_ENV !== 'production', // Disable during development
+            pngquant: {
+              quality: '95-100'
+            }
+          }),
+        
+        new WebpackMd5Hash(),
+        new webpack.DefinePlugin({
+            'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        })
+    ]
+};
